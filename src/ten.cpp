d23faf3943f883ec1790dcd98e5a18db08d035b1
Matteo <matteo.mainenti@studenti.univr.it>
2018-03-22 15:37:45 +0100

    src files

--- src/ten.cpp
+++ src/ten.cpp
@@ -28,20 +28,16 @@
 // %Tag(FULLTEXT)%
 #include "ros/ros.h"
 #include "std_msgs/String.h"
-#include "signal.h"
+
 /**
  * This tutorial demonstrates simple receipt of messages over the ROS system.
  */
 // %Tag(CALLBACK)%
-
-   int count=0;
 void chatterCallback(const std_msgs::String::ConstPtr& msg)
 {
   ROS_INFO("I heard: [%s]", msg->data.c_str());
-  count ++;
-
-// %EndTag(CALLBACK)%
 }
+// %EndTag(CALLBACK)%
 
 int main(int argc, char **argv)
 {
@@ -55,7 +51,7 @@ int main(int argc, char **argv)
    * You must call one of the versions of ros::init() before using any other
    * part of the ROS system.
    */
-  ros::init(argc, argv, "listener");
+  ros::init(argc, argv, "ten");
 
   /**
    * NodeHandle is the main access point to communications with the ROS system.
@@ -80,110 +76,18 @@ int main(int argc, char **argv)
    * away the oldest ones.
    */
 // %Tag(SUBSCRIBER)%
-
-   ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
-  // %EndTag(SUBSCRIBER)%
-
-    /**
-     * ros::spin() will enter a loop, pumping callbacks.  With this version, all
-     * callbacks will be called from within this thread (the main one).  ros::spin()
-     * will exit when Ctrl-C is pressed, or the node is shutdown by the master.
-     */
-  // %Tag(SPIN)%
-   ros::Rate cycle(10);
-bool partenza=0;
-while(count<10){
-
-    ros::spinOnce();
-  // %EndTag(SPIN)%
-   cycle.sleep();
-  
-}
- 
- 
-partenza=1;
-
- if(partenza)
- {
-
-
- ros::NodeHandle a;
-// %EndTag(NODEHANDLE)%
+  ros::Subscriber sub = n.subscribe("filter", 1000, chatterCallback);
+// %EndTag(SUBSCRIBER)%
 
   /**
-   * The advertise() function is how you tell ROS that you want to
-   * publish on a given topic name. This invokes a call to the ROS
-   * master node, which keeps a registry of who is publishing and who
-   * is subscribing. After this advertise() call is made, the master
-   * node will notify anyone who is trying to subscribe to this topic name,
-   * and they will in turn negotiate a peer-to-peer connection with this
-   * node.  advertise() returns a Publisher object which allows you to
-   * publish messages on that topic through a call to publish().  Once
-   * all copies of the returned Publisher object are destroyed, the topic
-   * will be automatically unadvertised.
-   *
-   * The second parameter to advertise() is the size of the message queue
-   * used for publishing messages.  If messages are published more quickly
-   * than we can send them, the number here specifies how many messages to
-   * buffer up before throwing some away.
+   * ros::spin() will enter a loop, pumping callbacks.  With this version, all
+   * callbacks will be called from within this thread (the main one).  ros::spin()
+   * will exit when Ctrl-C is pressed, or the node is shutdown by the master.
    */
-// %Tag(PUBLISHER)%
-  ros::Publisher chatter_pub = a.advertise<std_msgs::String>("filter", 1000);
-// %EndTag(PUBLISHER)%
+// %Tag(SPIN)%
+  ros::spin();
+// %EndTag(SPIN)%
 
-// %Tag(LOOP_RATE)%
-  ros::Rate loop_rate(10);
-// %EndTag(LOOP_RATE)%
-
-  /**
-   * A count of how many messages we have sent. This is used to create
-   * a unique string for each message.
-   */
-// %Tag(ROS_OK)%
-   count = 0;
-  while (ros::ok())
-  {
-// %EndTag(ROS_OK)%
-    /**
-     * This is a message object. You stuff it with data, and then publish it.
-     */
-// %Tag(FILL_MESSAGE)%
-    std_msgs::String msg;
-
-    std::stringstream ss;
-    ss << "hello world " << count;
-    msg.data = ss.str();
-// %EndTag(FILL_MESSAGE)%
-
-// %Tag(ROSCONSOLE)%
-    ROS_INFO("%s", msg.data.c_str());
-// %EndTag(ROSCONSOLE)%
-
-    /**
-     * The publish() function is how you send messages. The parameter
-     * is the message object. The type of this object must agree with the type
-     * given as a template parameter to the advertise<>() call, as was done
-     * in the constructor above.
-     */
-// %Tag(PUBLISH)%
-    chatter_pub.publish(msg);
-// %EndTag(PUBLISH)%
-
-// %Tag(SPINONCE)%
-    ros::spinOnce();
-// %EndTag(SPINONCE)%
-
-// %Tag(RATE_SLEEP)%
-    loop_rate.sleep();
-// %EndTag(RATE_SLEEP)%
-    ++count;
-  }
-
- }
-
-
-  
   return 0;
-
 }
 // %EndTag(FULLTEXT)%
